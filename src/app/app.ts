61ebb0c6b3f9e7edba17a6f09ded9ff32f4e8d7a
Tim van Heumen <tim@vhweb.nl>
2026-02-01 11:48:58 +0100

    Added ngx-markdown

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MarkdownComponent } from "ngx-markdown";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MarkdownComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
