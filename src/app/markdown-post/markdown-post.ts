ff59f3d9a8105b3f4fe1e0c6b64eaf9043bb4e3a
Tim van Heumen <tim@vhweb.nl>
2026-02-09 10:46:54 +0100

    Added header to post

--- src/app/markdown-post/markdown-post.ts
+++ src/app/markdown-post/markdown-post.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject, Input, OnInit, signal } from '@angular/core';
 import frontMatter from 'front-matter';
 import { MarkdownComponent } from "ngx-markdown";
+import { Attributes } from '../posts-overview/post-metadata';
 
 @Component({
   selector: 'app-markdown-post',
@@ -17,6 +18,7 @@ export class MarkdownPost implements OnInit {
   private httpClient = inject(HttpClient);
 
   protected readonly content = signal('');
+  protected readonly title = signal('');
 
   ngOnInit(): void {
     this.loadMarkdown(this.folderName, this.postName)
@@ -25,7 +27,9 @@ export class MarkdownPost implements OnInit {
   private loadMarkdown(folderName:string , postName: string) {
     this.httpClient.get(`markdown/posts/${folderName}/${postName}.md`,{responseType: 'text'}).subscribe((markdown) =>{
       const parseResult = frontMatter(markdown);
+      const attributes = parseResult.attributes as Attributes
       this.content.set(parseResult.body);
+      this.title.set(attributes.title);
 
       // TODO: handle 404
     });
